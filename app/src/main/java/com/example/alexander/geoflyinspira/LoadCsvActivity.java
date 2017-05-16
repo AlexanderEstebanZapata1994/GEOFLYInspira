87fcdc5057f631ce21a1a49312a582403614cf73
Alexander Esteban Zapata <alexanderesteban7@gmail.com>
2017-05-16 00:55:53 -0500

    Segunda Actividad diseño para cargar la información
    Se crea interfaz grafica para cargar allí la información, y guarde en la
    base de datos, adicionalmente se debe despejar duda acerca de como carga
    las imagenes

---
+++ app/src/main/java/com/example/alexander/geoflyinspira/LoadCsvActivity.java
@@ -0,0 +1,17 @@
+package com.example.alexander.geoflyinspira;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alexander Esteban on 5/15/2017.
+ */
+
+public class LoadCsvActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_csv);
+        //TODO Colocar el icono a los botones cargar
+    }
+}
--- app/src/main/java/com/example/alexander/geoflyinspira/MainActivity.java
+++ app/src/main/java/com/example/alexander/geoflyinspira/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.alexander.geoflyinspira;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +14,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    // ActionBar  methods
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.options_menu, menu);
+        // Getting the action bar item
+        MenuItem menuItem = (MenuItem) findViewById(R.id.action_load_info);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_load_info:
+                onCreateLoadCsvActivity();
+                return true;
+
+            case R.id.action_about:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.action_logout:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    private void onCreateLoadCsvActivity(){
+        Intent intent = new Intent(this, LoadCsvActivity.class);
+        startActivity(intent);
     }
 }

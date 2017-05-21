283ee7ad62f0cae39167c58ce21a83adb04f3729
Inpacto Facilita <inpactofacilita@gmail.com>
2017-05-20 22:18:29 -0500

    se crea la interfaz para el Acerca de

---
+++ app/src/main/java/com/example/alexander/geoflyinspira/AboutActivity.java
@@ -0,0 +1,10 @@
+package com.example.alexander.geoflyinspira;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yenifer on 20/05/2017.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+}

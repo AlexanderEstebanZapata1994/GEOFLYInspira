49d193128e5125f5fe6ebf4de3a96743c8a98897
Inpacto Facilita <inpactofacilita@gmail.com>
2017-05-23 23:21:50 -0500

    cambios en about

--- app/src/main/java/com/example/alexander/geoflyinspira/LoadCsvActivity.java
+++ app/src/main/java/com/example/alexander/geoflyinspira/LoadCsvActivity.java
@@ -1,28 +1,35 @@
 package com.example.alexander.geoflyinspira;
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.icu.text.DateTimePatternGenerator;
 import android.icu.text.SimpleDateFormat;
+import android.icu.util.TimeZone;
+import android.media.ImageReader;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.*;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.alexander.geoflyinspira.data.CoordenadaDbHelper;
-import com.example.alexander.geoflyinspira.data.coordenadaContract;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.*;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.*;
 import java.util.Date;
+import com.example.alexander.geoflyinspira.data.CoordenadaDbHelper;
+import com.example.alexander.geoflyinspira.data.coordenadaContract;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Alexander Esteban on 5/15/2017.
@@ -31,19 +38,39 @@ import java.util.Date;
 public class LoadCsvActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String SEPARATOR = ",";
-    public static final String QUOTE="\"";
-    public static final int FILE_SELECT_CODE = 0;
+    public static final int FILE_SELECTED_CODE = 0;
+    public static final int SELECTED_IMAGE = 1046;
     private CoordenadaDbHelper coordenadaDbHelper;
+    private TextView txt_path_file;
+    private Button btnLoad;
+    private Button btnLoadImg;
+    private ListView listView;
+    public Uri uri;
+    public Uri photoUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_csv);
-
-        Button btnLoad = (Button) findViewById(R.id.btn_loadFile);
-        btnLoad.setOnClickListener(this);
         coordenadaDbHelper = new CoordenadaDbHelper(this);
+
+        //Instanciamos los objetos que usaremos para interactuar entre la lógica y la GUI
+        btnLoad = (Button) findViewById(R.id.btn_loadFile);
+        btnLoadImg = (Button) findViewById(R.id.btn_loadImg);
+        txt_path_file = (TextView) findViewById(R.id.lbl_path_file);
+
+        // Establecemos los eventos para cada unos de los objetos instanciados previamente
+        btnLoad.setOnClickListener(this);
+        btnLoadImg.setOnClickListener(this);
     }
 
+    // ActionBar  methods
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_load_csv, menu);
+        // Getting the action bar item
+        MenuItem menuItem = (MenuItem) findViewById(R.id.action_inspired_photos);
+        return true;
+    }
     /**
      * Called when a view has been clicked.
      *
@@ -51,29 +78,42 @@ public class LoadCsvActivity extends AppCompatActivity implements View.OnClickLi
      */
     @Override
     public void onClick(View v) {
-        loadCsv();
+        if (v.getId() == btnLoad.getId()){
+            if (btnLoad.getText() == getResources().getString(R.string.btn_select_file)){
+                showFileChooser();
+            }else if (btnLoad.getText() == getResources().getString(R.string.btn_save)){
+                loadCsv(this.uri);
+            }
+        }
+        if (v.getId() == btnLoadImg.getId()) {
+            if (btnLoadImg.getText() == getResources().getString(R.string.btn_select_file)) {
+                showImageChooser();
+            } else if (btnLoadImg.getText() == getResources().getString(R.string.btn_save)) {
+                loadImg(this.uri);
+            }
+        }
     }
 
-    private void loadCsv (){
+    private void loadCsv (Uri uri){
         String isHeader = "";
         String isPhoto = "";
         String isVideo = "";
+        Date timestamp = null;
         float latitude = 0;
         float longitude = 0;
         float altitude = 0;
-        Date timestamp = null;
-
+        long newRowId = 0;
         BufferedReader br = null;
 
+        // Formateamos la fecha para la fecha de inserción del registro
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss", Locale.ENGLISH);
         // Creamos una instancia de la base de datos de tal forma que podamos escribir sobre ella.
         SQLiteDatabase db = coordenadaDbHelper.getWritableDatabase();
-        // Formateamos la fecha
-        SimpleDateFormat dateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss", Locale.ENGLISH);
-        Date date = null;
         try {
-            InputStream inputStream = this.getResources().openRawResource(R.raw.records);
+            InputStream inputStream = getContentResolver().openInputStream(uri);
             br = new BufferedReader(new InputStreamReader(inputStream));
             String line = br.readLine();
+            int i = 0;
             while (null != line) {
                 String[] fields = line.split(SEPARATOR);
                 line = br.readLine();
@@ -81,29 +121,31 @@ public class LoadCsvActivity extends AppCompatActivity implements View.OnClickLi
                 ContentValues values = new ContentValues();
                 // Validamos que si la primer fila tiene los encabezado entonces siga con la siguiente fila
                 isHeader = fields[0].toString();
-                latitude = Float.parseFloat(fields[0].toString());
-                longitude = Float.parseFloat(fields[0].toString());
-                altitude = Float.parseFloat(fields[0].toString());
-                isPhoto = fields[15].toString();
-                isVideo = fields[16].toString();
 
                 if (!isHeader.equals("latitude")){
+                    latitude = Float.parseFloat(fields[0].toString());
+                    longitude = Float.parseFloat(fields[1].toString());
+                    altitude = Float.parseFloat(fields[2].toString());
+                    isPhoto = fields[15].toString();
+                    isVideo = fields[16].toString();
+
                     if ( (isPhoto.equals("1")) || (isVideo.equals("1")) ) {
                         values.put(coordenadaContract.COORDENADAEntry.COL_COOR_LATITUD, latitude );
                         values.put(coordenadaContract.COORDENADAEntry.COL_COOR_LONGITUD, longitude );
                         values.put(coordenadaContract.COORDENADAEntry.COL_COOR_ALTITUD, altitude );
-                        // TODO pendiente por guardar fecha del archivo ERROR: Unparsable java.text.ParseException: Unparseable date
-                        //Date parsedDate = dateFormat.parse(fields[11].toString ());
-                        SimpleDateFormat print = new SimpleDateFormat("MM d, yyyy HH:mm:ss");
-                        //values.put(coordenadaContract.COORDENADAEntry.COL_COOR_DATETIME, print.format(parsedDate) ); // Datetime
+                        // TODO  1 pendiente por guardar fecha del archivo ERROR: Unparsable java.text.ParseException: Unparseable date
+                        timestamp  = getCastingStrToDate(fields[11].toString (), "dd-MM-yyyy");
+                        values.put(coordenadaContract.COORDENADAEntry.COL_COOR_DATETIME, dateFormat.format(timestamp)); // Datetime
+                        values.put(coordenadaContract.COORDENADAEntry.COL_COOR_CREATED_DATE, dateFormat.format(new Date()));
 
-                        values.put(coordenadaContract.COORDENADAEntry.COL_COOR_CREATED_DATE, dateFormat.format(new Date())); // CREATED DATE
                         // Insertamos los datos, si la inserción nos devuelve un -1 quiere decir que hubo un error en la inserción
                         // De lo contrario guardará la información de forma correcta.
-                        long newRowId = db.insert(coordenadaContract.COORDENADAEntry.TABLE_NAME, null, values);
+                        newRowId = db.insert(coordenadaContract.COORDENADAEntry.TABLE_NAME, null, values);
+                        i++;
                     }
                 }
             }
+            Toast.makeText(this, String.valueOf(i) +  " registros guardados exitosamente.", Toast.LENGTH_SHORT).show();
         }catch (Exception e){
             System.out.print(e.getMessage());
         }finally {
@@ -117,47 +159,101 @@ public class LoadCsvActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    private static String[] removeTrailingQuotes(String[] fields) {
-        String result[] = new String[fields.length];
-
-        for (int i=0;i<result.length;i++){
-            result[i] = fields[i].replaceAll("^"+QUOTE, "").replaceAll(QUOTE+"$", "");
+    private void showFileChooser() {
+        // Creamos un intento para abrir la aplicación
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        // Que permita el abrir el archivo
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/csv");      //csv only files
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Seleccione un archivo CSV para cargar"), FILE_SELECTED_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "Por favor, instale un administrador de archivos.", Toast.LENGTH_SHORT).show();
         }
-        return result;
     }
 
-    private void showFileChooser() {
+    private void loadImg(Uri uri){
 
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/csv");      //all files
-        //intent.setType("text/xml");   //XML file only
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
+    }
+
+    private void showImageChooser() {
+
+        // Creamos un intento para abrir la aplicación
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        // Que permita el abrir el archivo
+        //intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*"); // png, jpg, bitmap files
+        // TODO Capturar varias imagenes
+        // https://guides.codepath.com/android/Accessing-the-Camera-and-Stored-Media
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Seleccione las imágenes para cargar"), SELECTED_IMAGE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "Por favor, instale un administrador de archivos.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData){
+        //Variables para el archivo cargado
+        String path = "";
+
+        switch (requestCode){
+            case FILE_SELECTED_CODE:
+                // Si se selecciona un archivo
+                if(resultCode == Activity.RESULT_OK){
+                    // ResultData obtenemos el archivo gargado validando que no sea null
+                    if(resultData != null){
+                        this.uri = resultData.getData();
+                        // Establecemos la ruta del archivo
+                        path = this.uri.toString();
+                        // TODO 2 Obtener el nombre del archivo para la ruta.
+                        txt_path_file.setText(path);
+                        btnLoad.setText(R.string.btn_save);
+                    }
+                }else{ // Si no selecciona nada
+                    Toast.makeText(this, "No selecciono ningún archivo", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case SELECTED_IMAGE:
+                // Si se selecciona un archivo
+                if(resultCode == Activity.RESULT_OK){
+                    // ResultData obtenemos el archivo gargado validando que no sea null
+                    if(resultData != null){
+                        this.photoUri = resultData.getData();
+                        // Establecemos la ruta del archivo
+                        path = this.photoUri.getLastPathSegment();
+                        path = path + "/"  ;
+                        btnLoadImg.setText(R.string.btn_save);
+                    }
+                }else {
+                    Toast.makeText(this, "No selecciono ninguna imagen", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    /**
+     * @param date = Fecha en formato String para que sea convertida
+     * @param format = Formato especificado para el cual sera convertida la fecha,
+     *               si no se especifica el formato por defecto sera MM d, yyyy HH:mm:ss
+     * @return convertedDate = Sera la fecha de salida ya formateada.
+     * */
+    protected Date getCastingStrToDate(String date, String format){
+        Date convertedDate = new Date();
+        SimpleDateFormat inputFormat = new SimpleDateFormat(format, Locale.ENGLISH);
+        inputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        SimpleDateFormat outputFormat = new SimpleDateFormat(format, Locale.ENGLISH);
 
         try {
-            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
-        } catch (android.content.ActivityNotFoundException ex) {
-            // Potentially direct the user to the Market with a Dialog
-            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+            convertedDate = inputFormat.parse(date);
+        }catch (Exception e){
+            e.getStackTrace();
         }
+        return convertedDate;
     }
 
-    // Este metodo es para visualizar la información de la base de datos creada y
-    // poder verificar que efectivamente fue creada y tenemos conexión con la bd
-
-    private void displayDatabaseInfo(){
-        coordenadaDbHelper = new CoordenadaDbHelper(this);
-
-        SQLiteDatabase database =coordenadaDbHelper.getReadableDatabase();
-
-        Cursor cursor = database.rawQuery("SELECT * FROM " + coordenadaContract.COORDENADAEntry.TABLE_NAME, null);
-        try{
-            TextView txt = (TextView) findViewById(R.id.lbl_path_file);
-            txt.setText("ROWS " + cursor.getCount());
-        }finally {
-            cursor.close();
-        }
-    }
-    /*
+    /**
     Since getWritableDatabase() and getReadableDatabase() are expensive to call when the database is closed,
     you should leave your database connection open for as long as you possibly need to access it.
     Typically, it is optimal to close the database in the onDestroy() of the calling Activity.
@@ -167,5 +263,32 @@ public class LoadCsvActivity extends AppCompatActivity implements View.OnClickLi
     protected void onDestroy(){
         coordenadaDbHelper.close();
         super.onDestroy();
+    }
+
+    /****************************** Menu de opciones ACTION BAR ***************************************/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_inspired_photos:
+                onCreateInspiredPhotosActivity();
+                return true;
+
+            case R.id.action_about:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.action_logout:
+                System.exit(0);
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void onCreateInspiredPhotosActivity(){
+        // TODO Crear la interfaz para las fotos inspiradoras
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
